f72b7c48c236c742502641f8a7b315ae6c2eca4b
Yaroslava Ponomareva <ponomareva@friendsonly.me>
2024-10-29 18:00:18 +0300

    docs: add the basic strucure of the markdown cv

---
+++ cv.md.m
@@ -0,0 +1,21 @@
+# Yaraslava Panamarova
+
+## Contacts
+[Link](https://github.com/Gordislava)
+[Link](https://www.linkedin.com/in/yaraslava-panamarova/)
+
+---
+
+## About
+
+---
+
+## Skills
+
+---
+
+## Code Examples
+
+---
+
+## Languages
